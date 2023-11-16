0679c88c8843ab513ed37d3664b668fd6eca63bb
The Captaan <118829565+thecaptaan@users.noreply.github.com>
2023-11-16 22:43:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -326,9 +326,6 @@ function portfolioScreen() {
     },
   ];
   let portfolioHtml = `
-    <div class="portfolio-header">
-        <h2></h2>
-    </div>
     <div class="portfolio-tags-container row">
         <span class="portfolio-tag">All</span>
         <span class="portfolio-tag">Web Development</span>
@@ -371,17 +368,15 @@ function blogScreen() {
   screenHeader.innerHTML = `<h1>Blogs</h1>`;
   let data = [
     {
-      img: "./assets/images/coming-soon.jpg",
-      title: "Coming Soon",
+      img: "./assets/images/web-developer.jpg",
+      title: "Express JS For Beginners",
       sub: "Web Development",
-      link: "",
-      description: "",
+      link: "https://dev.to/thecaptaan/express-js-for-beginners-5cl8",
+      description:
+        "Explore the fundamentals of web development with Express.js in this beginner-friendly guide, covering the basics of routing, middleware, and building RESTful APIs using the popular Node.js framework",
     },
   ];
   let blogHtml = `
-    <div class="blog-header">
-        <h2></h2>
-    </div>
     <div class="blog-tags-container row">
         <span class="blog-tag">All</span>
         <span class="blog-tag">Web Development</span>
@@ -427,23 +422,86 @@ function contactScreen() {
       style="filter: invert(90%) hue-rotate(180deg)" title="Map of Bettiah, Bihar - Bharat"
       referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-    
-    <div class="contact-container row">
-        <div class="contact-card col">
-            <div class="contact-card-header">
-                <span>Get in Touch</span>
-            </div>
-            <div class="contact-card-content">
-                <div class="contact-card-content-item row">
-                    <img src="./assets/images/email.png" width="20px" height="20px" alt="" />
-                    <span>
-                        <a href="mailto:
-                        " target="_blank">Email</a>
-                        </span>
-                </div>
-            </div>
-        </div>
+    <br />
+    <br />
+    <div>
+        <h2>Get In Touch</h2>
     </div>
+    <div class="contact-card-container row">
+                <a class="aside-card row" href="mailto:thecaptaan@proton.me">
+                    <div class="aside-icon-cont">
+                        <svg id="aside-icon" height="200px" width="200px" version="1.1" id="_x32_"
+                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            viewBox="0 0 512 512" xml:space="preserve" fill="#000000">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <style type="text/css">
+                                    .st0 {
+                                        fill: #ffdb70;
+                                    }
+                                </style>
+                                <g>
+                                    <path class="st0"
+                                        d="M510.746,110.361c-2.128-10.754-6.926-20.918-13.926-29.463c-1.422-1.794-2.909-3.39-4.535-5.009 c-12.454-12.52-29.778-19.701-47.531-19.701H67.244c-17.951,0-34.834,7-47.539,19.708c-1.608,1.604-3.099,3.216-4.575,5.067 c-6.97,8.509-11.747,18.659-13.824,29.428C0.438,114.62,0,119.002,0,123.435v265.137c0,9.224,1.874,18.206,5.589,26.745 c3.215,7.583,8.093,14.772,14.112,20.788c1.516,1.509,3.022,2.901,4.63,4.258c12.034,9.966,27.272,15.45,42.913,15.45h377.51 c15.742,0,30.965-5.505,42.967-15.56c1.604-1.298,3.091-2.661,4.578-4.148c5.818-5.812,10.442-12.49,13.766-19.854l0.438-1.05 c3.646-8.377,5.497-17.33,5.497-26.628V123.435C512,119.06,511.578,114.649,510.746,110.361z M34.823,99.104 c0.951-1.392,2.165-2.821,3.714-4.382c7.689-7.685,17.886-11.914,28.706-11.914h377.51c10.915,0,21.115,4.236,28.719,11.929 c1.313,1.327,2.567,2.8,3.661,4.272l2.887,3.88l-201.5,175.616c-6.212,5.446-14.21,8.443-22.523,8.443 c-8.231,0-16.222-2.99-22.508-8.436L32.19,102.939L34.823,99.104z M26.755,390.913c-0.109-0.722-0.134-1.524-0.134-2.341V128.925 l156.37,136.411L28.199,400.297L26.755,390.913z M464.899,423.84c-6.052,3.492-13.022,5.344-20.145,5.344H67.244 c-7.127,0-14.094-1.852-20.142-5.344l-6.328-3.668l159.936-139.379l17.528,15.246c10.514,9.128,23.922,14.16,37.761,14.16 c13.89,0,27.32-5.032,37.827-14.16l17.521-15.253L471.228,420.18L464.899,423.84z M485.372,388.572 c0,0.803-0.015,1.597-0.116,2.304l-1.386,9.472L329.012,265.409l156.36-136.418V388.572z">
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="col">
+                        <span class="aside-card-header">Email</span>
+                        <span>thecaptaan@proton.me</span>
+                    </div>
+                </a>
+                <a class="aside-card row" href="https://maps.app.goo.gl/1V21ToDGpbNAEdjn9">
+                    <div class="aside-icon-cont">
+                        <svg id="aside-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M19.925 1.78443C21.5328 1.18151 23.1029 2.75156 22.5 4.35933L16.0722 21.5C15.3574 23.4061 12.5838 23.1501 12.23 21.1453L10.8664 13.418L3.1391 12.0544C1.13427 11.7006 0.878261 8.92697 2.78443 8.21216L19.925 1.78443ZM20.6273 3.65708L3.48668 10.0848L11.2139 11.4485C12.0417 11.5945 12.6898 12.2426 12.8359 13.0704L14.1996 20.7977L20.6273 3.65708Z"
+                                    fill="#ffdb70"></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="col">
+                        <span class="aside-card-header">Address</span>
+                        <span>Bettiah, Bihar - Bharat</span>
+                    </div>
+                </a>
+                <a class="aside-card row"
+                    href="https://api.whatsapp.com/send?phone=6377192121&text=%22Hi%2C%20I%20am%20reaching%20out%20through%20your%20portfolio%20website.%20I%20am%20interested%20in%20establishing%20a%20partnership%20with%20your%20startup">
+                    <div class="aside-icon-cont">
+                        <svg id="aside-icon" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#ffdb70">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g id="Layer_2" data-name="Layer 2">
+                                    <g id="invisible_box" data-name="invisible box">
+                                        <rect width="48" height="48" fill="none"></rect>
+                                    </g>
+                                    <g id="Icons">
+                                        <g>
+                                            <path
+                                                d="M38.9,8.1A20.9,20.9,0,0,0,3.2,22.8,19.8,19.8,0,0,0,6,33.2L3,44l11.1-2.9a20.3,20.3,0,0,0,10,2.5A20.8,20.8,0,0,0,38.9,8.1Zm-14.8,32a17.1,17.1,0,0,1-9.5-2.8L8,39.1l1.8-6.4a17.9,17.9,0,0,1-3.1-9.9A17.4,17.4,0,1,1,24.1,40.1Z">
+                                            </path>
+                                            <path
+                                                d="M33.6,27.2A29.2,29.2,0,0,0,30,25.5c-.4-.2-.8-.3-1.1.2s-1.4,1.7-1.7,2.1a.8.8,0,0,1-1.1.1,15.2,15.2,0,0,1-4.2-2.6A15,15,0,0,1,19,21.7a.7.7,0,0,1,.2-1l.8-1a3.5,3.5,0,0,0,.5-.8.9.9,0,0,0,0-.9c-.2-.3-1.2-2.8-1.6-3.9s-.9-.9-1.2-.9h-1a1.7,1.7,0,0,0-1.4.7,5.5,5.5,0,0,0-1.8,4.3,10.4,10.4,0,0,0,2.1,5.4c.3.3,3.7,5.6,8.9,7.8a16.4,16.4,0,0,0,3,1.1,6.4,6.4,0,0,0,3.3.2c1-.1,3.1-1.2,3.5-2.4s.5-2.3.3-2.5A2.1,2.1,0,0,0,33.6,27.2Z">
+                                            </path>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    <div class="col">
+                        <span class="aside-card-header">Whatsapp</span>
+                        <span>+91 6377192121</span>
+                    </div>
+                </a>
+            </div>
                         `;
   mainContent.innerHTML = contactHtml;
 }
